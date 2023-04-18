8ab26e8a783c58d231a8d6764acb0fa28cc5924f
ArleysonCosta <arleycaio@gmail.com>
2023-04-18 20:30:45 -0300

    Foi criado a rota depositar

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ fastify.get('/saldo', (req, res) => {
     res.status(200).send(`Seu saldo é ${saldo}`);
 });
 
+fastify.post('/depositar', (req,res) =>{
+    let valor = Number(req.body.valor);
+    saldo = saldo + valor;
+    res.status(201).send(`Valor depositado com sucesso`)
+});
+
 fastify.listen(PORT, (err) => {
     if (err) throw err;
     console.log(`Servidor rodando na porta ${PORT}`)
