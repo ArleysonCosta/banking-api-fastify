e54e349f612410d3cc19543a19594bf1e2013aaa
ArleysonCosta <arleycaio@gmail.com>
2023-04-18 21:04:52 -0300

    Concluida as funcionalidades transferencia e encerrar conta

--- index.js
+++ index.js
@@ -8,10 +8,28 @@ fastify.get('/saldo', (req, res) => {
     res.status(200).send(`Seu saldo é ${saldo}`);
 });
 
+fastify.put('/saque', (req,res) => {
+    let valor = Number(req.body.valor);
+    saldo = saldo - valor;
+    res.status(202).send(`Saque realizado com sucesso, seu novo saldo é ${saldo}`);    
+});
+
 fastify.post('/depositar', (req,res) =>{
     let valor = Number(req.body.valor);
     saldo = saldo + valor;
-    res.status(201).send(`Valor depositado com sucesso`)
+    res.status(201).send(`Valor depositado com sucesso,seu novo saldo é ${saldo}`)
+});
+
+fastify.patch('/tranferencia', (req,res)=>{
+    let valor = Number(req.body.valor);
+    let destino = req.body.destino;
+    let saldo = saldo - valor;
+    res.status(202).send(`tranferencia do valor ${valor} realizada para o ${destino}, saldo atual é ${saldo}.`)
+})
+
+fastify.delete('/encerrar', (req,res) =>{
+    saldo = 0;
+    res.status(205).send('Conta Encerrada');    
 });
 
 fastify.listen(PORT, (err) => {
